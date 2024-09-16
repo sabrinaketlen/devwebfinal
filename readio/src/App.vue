df99549eaf27303a5bca94eec6e09f3246492bed
Joaquim <sabrinaketlencs2003@gmail.com>
2024-09-16 15:29:46 -0300

    header

---
+++ readio/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="conteiner">
+    <myHeader></myHeader>
+  </div>
+</template>
+
+<script>
+import myHeader from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: { myHeader }
+}
+</script>
+
+<style>
+.conteiner {
+  display: flex;
+  flex-direction: column;
+}
+</style>

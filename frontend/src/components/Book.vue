0525aecb95a70d6a03ef5d253b6330b6f5233162
Joaquim <sabrinaketlencs2003@gmail.com>
2024-09-21 22:52:13 -0300

    book style

--- frontend/src/components/Book.vue
+++ frontend/src/components/Book.vue
@@ -13,26 +13,38 @@ console.log(route.params)
 </script>
 
 <template>
-    <div class="col">
+    <div class="col card-style">
       <div class="card shadow-sm">
-        <img 
-          v-if="props.capa" 
-          :src="uploadHelper(props.capa.data.attributes.url)" 
-          :alt="`Capa do Livro ${props.nome}`" 
+        <img
+          v-if="props.capa"
+          :src="uploadHelper(props.capa.data.attributes.url)"
+          :alt="`Capa do Livro ${props.nome}`"
           class="cover"
         />
         <div class="card-body">
-          <h5 class="card-title">{{ props.nome }} </h5>
-          <p class="card-text"> {{ props.autor }}</p>
+          <h5 class="card-title">{{ props.nome }}</h5>
+          <p class="card-text">{{ props.autor }}</p>
         </div>
       </div>
     </div>
-  
-  
 </template>
 
 <style scoped>
 .cover:hover {
   filter: sepia(0.8);
+}
+
+.card-style {
+  padding: 10px;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+  transition: 0.5s;
+}
+
+.card-style:hover {
+  transform: scale(1.12);
+  transition: 0.5s;
+  padding-bottom: 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.8);
 }
 </style>

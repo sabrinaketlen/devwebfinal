61bc21399c094d763e62c4446968e114c9c3c80d
Joaquim <sabrinaketlencs2003@gmail.com>
2024-09-25 16:11:42 -0300

    busca estante corrigida

--- frontend/src/pages/Estante.vue
+++ frontend/src/pages/Estante.vue
@@ -11,7 +11,6 @@ console.log("Componente renderizado");
 
 const userStore = useUserStore();
 const estante = ref({} as Estante);
-const estantes = ref([] as Estante[]);
 const exception = ref<ApplicationError>();
 const loading = ref(true);
 const user_id = userStore.user.id;
@@ -21,52 +20,15 @@ console.log("Token JWT:", userStore.jwt);
 async function getEstantes(){
   console.log("getEstantes chamada");
   try {
-    const { data } = await api.get(`/estantes?populate=livros.Capa, users_permissions_user`, {
+    const { data } = await api.get(`/estantes?populate=livros.Capa, users_permissions_user&filters[users_permissions_user][id]=${user_id}`, {
       headers: {
         Authorization: `Bearer ${userStore.jwt}`,
       },
     });
-    console.log(data.data)
-    estantes.value = data.data.map((estante: any) => ({
-      id: estante.id,
-      user: {
-        id: estante.attributes.users_permissions_user.data.id,
-        username: estante.attributes.users_permissions_user.data.attributes.username,
-        role: estante.attributes.users_permissions_user.data.attributes.role,
-        email: estante.attributes.users_permissions_user.data.attributes.email,
-      },
-      livros: estante.attributes.livros.data.map((livro: any) => ({
-        id: livro.id,
-        Nome: livro.attributes.Nome,
-        Autor: livro.attributes.Autor,
-        Genero: livro.attributes.Genero,
-        Sinopse: livro.attributes.Sinopse,
-        Capa:
-           {
-              id: livro.attributes.Capa.data.id,
-              url: livro.attributes.Capa.data.attributes.url,
-            },
-        Nota: livro.attributes.Nota,
-        nCapitulos: livro.attributes.nCapitulos,
-        
-      })),
-    }));
-    console.log(estantes.value.length);
 
-    console.log(estantes.value)
-    for(let i = 0; i < estantes.value.length; i++){
-      if(estantes.value[i].user.id == user_id){
-        estante.value = estantes.value[i]
-      }
-    }
-
-    console.log(estante);
-    //estante.value = data.data.attributes.livros.data
-    //console.log(data.data.attributes.livros.data);
-
-    if (data.data.length === 0) {
-      throw new Error("Estante não encontrada para o usuário");
-    }
+    console.log(data.data[0])
+    estante.value = data.data[0]
+    console.log(estante.value)
   } catch (e) {
     if (isAxiosError(e) && isApplicationError(e.response?.data)) {
       exception.value = e.response?.data;
